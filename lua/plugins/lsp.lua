3dadb1961c346a3a678464f4001d80b32e72c13c
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-11-01 20:49:29 +0100

    add `rust-analyzer` language server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,7 +57,23 @@ return {
                 --     mason = false,
                 --     cmd = { vim.fn.expand("~/.asdf/shims/ruby_lsp") },
                 -- },
-                -- rust_analyzer = {},
+                -- Rust
+                rust_analyzer = {
+                    imports = {
+                        granularity = {
+                            group = "module",
+                        },
+                        prefix = "self",
+                    },
+                    cargo = {
+                        buildScripts = {
+                            enable = true,
+                        },
+                    },
+                    procMacro = {
+                        enable = true
+                    },
+                },
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
                 -- Some languages (like typescript) have entire language plugins that can be useful:
