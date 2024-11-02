4391dfcdc0b5acf87822cba4667dc9fceb49b9da
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-11-02 22:28:30 +0100

    formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,7 +45,10 @@ return {
             { "nvim-telescope/telescope-ui-select.nvim" },
 
             -- Useful for getting pretty icons, but requires a Nerd Font.
-            { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+            {
+                "nvim-tree/nvim-web-devicons",
+                enabled = vim.g.have_nerd_font
+            },
         },
         config = function()
             -- NOTE: Run `:Telescope help_tags`
@@ -81,7 +84,7 @@ return {
 
             -- See `:help telescope.builtin`
             local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader><leader>",  builtin.find_files, { desc = "[S]earch [F]iles" })
+            vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "[S]earch [F]iles" })
             vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
             vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
             vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
